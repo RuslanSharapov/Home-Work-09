5a005f27f51e400b5702eeef4bd9280ca891f2eb
Ruslan_Sharapov <Enzyme@bk.ru>
2022-11-13 17:04:18 +0300

    Показать натуральные числа от M до N, N и M заданы

---
+++ work 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void PrintNumbers(int m, int n)
+{
+    if (m > n) return;
+    PrintNumbers(m, n - 1);
+    Console.Write($"{n} ");
+}
+PrintNumbers(1, 10);

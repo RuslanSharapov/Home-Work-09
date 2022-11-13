f07b4ec4422e21b7c8322bd7919a47225d4120c7
Ruslan_Sharapov <Enzyme@bk.ru>
2022-11-13 17:47:19 +0300

    Найти сумму элементов от M до N, N и M заданы

---
+++ work 2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumElements(int m, int n)
+{
+    if (m == n) return n;
+    return m + SumElements(m + 1, n);
+}
+Console.WriteLine($"Сумма элементов = {SumElements(1, 10)} ");
